17953cc931fbd6c33814500354c6bc13a411bbec
c-noblet <c.noblet.dev@gmail.com>
2022-03-20 17:15:21 +0100

    update release script

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 read -e -p "Enter the release version : " VERSION
 yarn run build:android && \
 cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ./bin/papagei.apk && \
+git commit -m "$VERSION pre release";
 git checkout develop && \
 git flow release start $VERSION && \
 git flow release finish $VERSION
